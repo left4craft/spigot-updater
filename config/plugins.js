2c0fe88dda4735c5dc852da1b99a1d626a98b639
Isaac (eartharoid) <contact@eartharoid.me>
2020-12-24 12:58:38 +0000

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,33 +1,39 @@
 module.exports = {	
-	spartan: {
+	Spartan: {
 		source: 'spigot', // spigot | github | direct
 		url: 'https://www.spigotmc.org/resources/spartan-advanced-anti-cheat-hack-blocker.25638/',
 		jar: 'Spartan.jar'
 	},
-	btlp_bungee: {
+	BTLP_Bungee: {
 		source: 'spigot',
 		url: 'https://www.spigotmc.org/resources/bungeetablistplus.313/',
 		// zip_path: 'BungeeTabListPlus_Bungee.jar', // exact name
 		zip_path: /BungeeTabListPlus-\S*\.jar/g, // regex for versioned names
 		jar: 'BTLP-Bungee.jar'
 	},
-	btlp_bukkit: {
+	BTLP_Bukkit: {
 		source: 'spigot',
 		url: 'https://www.spigotmc.org/resources/bungeetablistplus.313/',
 		// zip_path: 'BungeeTabListPlus_BukkitBridge.jar', // exact name
 		zip_path: /BungeeTabListPlus_BukkitBridge-\S*\.jar/g, // regex for versioned names
-		jar: 'BTLP-Bungee.jar'
+		jar: 'BTLP-Bukkit.jar'
 	},
-	left4chat: {
+	Left4Chat: {
 		source: 'github',
 		repository: 'left4craft/left4chat',
 		asset: 'Left4Chat.jar',
 		jar: 'Left4Chat.jar'
 	},
-	essentialsx: {
+	EssentialsX: {
 		source: 'github',
 		repository: 'essentialsx/essentials',
 		asset: /EssentialsX-\S*\.jar/g, // regex for versioned names,
 		jar: 'EssentialsX.jar'
+	},
+	ProtocolLib: {
+		source: 'github',
+		repository: 'dmulloy2/protocollib',
+		asset: 'ProtocolLib.jar',
+		jar: 'ProtocolLib.jar'
 	},
 };
