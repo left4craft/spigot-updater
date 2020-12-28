efb54f58cc5a2c44974aa7166261bda42ed662e2
Isaac (eartharoid) <contact@eartharoid.me>
2020-12-28 23:56:58 +0000

    update checker

---
+++ src/paper/check.js
@@ -0,0 +1,8 @@
+const fetch = require('node-fetch');
+const servers = require('../../config/servers');
+
+module.exports = bot => {
+	for (s in servers) {
+		
+	}
+};
--- src/updater/checkForUpdates.js
+++ src/updater/checkForUpdates.js
@@ -1,4 +1,11 @@
+
 module.exports = async (bot) => {
 	bot.log.info('Checking for server jar updates');
+
+	if (bot.config.server_jars_api.toLowerCase() === 'papermc')
+		require('../paper/check')(bot);
+	else
+		require('../serverjars/check')(bot);
+
 	bot.log.info('Checking for plugin updates');
 };
