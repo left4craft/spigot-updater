243dc4b38e7795ef0eee642b266ec149b2dabf1d
Isaac (eartharoid) <contact@eartharoid.me>
2020-12-28 12:57:26 +0000

    Move everything into the Bot class

--- src/index.js
+++ src/index.js
@@ -5,25 +5,9 @@
  */
 
 require('dotenv').config();
+const Logger = require('leekslazylogger');
 const fs = require('fs');
 const { path } = require('./utils/fs');
-
-const config = require('../config/config');
-
-const Logger = require('leekslazylogger');
-const log = new Logger({
-	name: 'Server updater',
-	debug: config.debug,
-	logToFile: config.save_logs
-});
-
-if (!fs.existsSync(path('data/downloads')))
-	fs.mkdirSync(path('data/downloads')),
-	log.console('Downloads directory not found, creating it for you...');
-
-if (!fs.existsSync(path('data/plugins')))
-	fs.mkdirSync(path('data/plugins')),
-	log.console('Plugins directory not found, creating it for you...');
 
 const { Client: DiscordClient } = require('discord.js');
 
@@ -32,38 +16,48 @@ class Bot extends DiscordClient {
 		super({
 			autoReconnect: true,
 		});
-		Object.assign(this, {
-			config,
-			log
+
+		this.config = require('../config/config');
+		this.log = new Logger({
+			name: 'Server updater',
+			debug: this.config.debug,
+			logToFile: this.config.save_logs
+		});
+
+		if (!fs.existsSync(path('data/downloads')))
+			fs.mkdirSync(path('data/downloads')),
+			this.log.console('Downloads directory not found, creating it for you...');
+
+		if (!fs.existsSync(path('data/plugins')))
+			fs.mkdirSync(path('data/plugins')),
+			this.log.console('Plugins directory not found, creating it for you...');
+
+		this.log.info('Connecting to Discord API');
+
+		this.on('ready', async () => {
+			this.log.success(`Authenticated as ${this.user.tag}`);
+
+			this.channel = this.channels.cache.get(this.config.channel_id);
+			if (!this.channel)
+				this.log.warn(`Could not get channel with ID of ${this.config.channel_id} in channels cache`);
+
+			const Updater = require('./updater');
+			const updater = new Updater(this);
+
+			await updater.check();
+			await updater.download();
+			await updater.run();
+
+			setInterval(() => updater.check(), 86400000); // check every day
+			setInterval(() => updater.download(), 3600000); // download every hour
+			setInterval(() => updater.run(), 43200000); // run every 12 hours
+		});
+
+		this.on('messageReactionAdd', (r, u) => {
+			this.log.console('Message reaction event received');
 		});
 	}
 }
 
 const bot = new Bot();
-
-log.info('Connecting to Discord API');
-
-bot.on('ready', async () => {
-	log.success(`Authenticated as ${bot.user.tag}`);
-	// bot.channels.cache.get(config.channel_id).send('✅ Started');
-
-	const Updater = require('./updater');
-	const updater = new Updater(bot);
-
-	await updater.check();
-	await updater.download();
-	await updater.run();
-
-	setInterval(() => updater.check(), 86400000); // check every day
-	setInterval(() => updater.download(), 3600000); // download every hour
-	setInterval(() => updater.run(), 43200000); // run every 12 hours
-});
-
-bot.on('messageReactionAdd', (r, u) => {
-	log.console('Message reaction event received');
-});
-
 bot.login();
-
-
-// 69 ;)
