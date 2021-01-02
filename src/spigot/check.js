86dc1fee73ab6aa04661a6d674b9adc032df6b2e
Isaac (eartharoid) <contact@eartharoid.me>
2021-01-02 20:56:46 +0000

    xenForo is dumb

--- src/spigot/check.js
+++ src/spigot/check.js
@@ -24,14 +24,13 @@ module.exports = async bot => {
 
 	bot.log.console('Loading page (waiting for Cloudflare)');
 	await page.goto('https://www.spigotmc.org/login');
-	await page.waitForTimeout(5000);
+	await page.waitForTimeout(10000);
 	// await page.waitForNavigation();
 	await page.screenshot({ path: 'loaded.png', fullPage: true });
 	bot.log.console('Logging into SpigotMC');
-	await page.type('#ctrl_pageLogin_login', process.env.SPIGOT_EMAIL); // , { delay: 100 } delay to make it more human-like
+	await page.type('#ctrl_pageLogin_login', process.env.SPIGOT_EMAIL);
 	await page.keyboard.press('Tab');
 	await page.keyboard.type(process.env.SPIGOT_PASSWORD);
-	// await page.type('#ctrl_pageLogin_password', process.env.SPIGOT_PASSWORD, { delay: 100 });
 	await page.keyboard.press('Tab');
 	await page.keyboard.press('Enter');
 	await page.waitForNavigation();
@@ -40,30 +39,25 @@ module.exports = async bot => {
 	let plugins = {};
 	Object.keys(bot.config.plugins)
 		.filter(plugin => bot.config.plugins[plugin].source.toLowerCase() === 'spigot')
-		// .map(plugin => bot.config.plugins[plugin]);
 		.forEach(plugin => plugins[plugin] = bot.config.plugins[plugin]);
 		
 	for (const plugin in plugins) {
 		await page.goto(`https://www.spigotmc.org/resources/${plugins[plugin].resource}/updates`);
 		
-		let updateID;
+		let version;
 		try {	
-			updateID = await page.evaluate(() => {
-				// eslint-disable-next-line no-undef
-				let update = document.querySelector('.updateContainer > ol').firstElementChild;
-				// return update.querySelector('h2 > a').href;
-				return update.id.substring(7);
-			});
+			// eslint-disable-next-line no-undef
+			const url = await page.evaluate(() => document.querySelector('.downloadButton > a').href);
+			version = (new URL(url)).searchParams.get('version');
+			if (!version) {
+				bot.log.warn(`Couldn't find a version number for ${plugin}`);
+				continue;
+			}
 		} catch (e) {
 			bot.log.error(e);
 		}
 		
-		// let version = (new URLSearchParams(url)).get('version');
-		if (!updateID) {
-			bot.log.warn(`Couldn't find a version number for ${plugin}`);
-			continue;
-		}
-		console.log(updateID);
 	}	
+
 	// await browser.close();	
 };
