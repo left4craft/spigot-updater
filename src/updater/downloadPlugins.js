da6ce8718b2989a8c4806a8447227cda4ecc9cd3
Trevor Wong <trevor8568@gmail.com>
2022-07-18 21:52:13 -0700

    improve error handling

--- src/updater/downloadPlugins.js
+++ src/updater/downloadPlugins.js
@@ -1,6 +1,23 @@
 module.exports = async (bot) => {
 	bot.log.info('Downloading plugins');
-	await require('../spigot/download')(bot);
-	await require('../github/download')(bot);
-	await require('../jenkins/download')(bot);
+	try {
+		await require('../spigot/download')(bot);
+	} catch (e) {
+		console.warn('Error downloading from spigot');
+		console.error(e);
+	}
+	
+	try {
+		await require('../github/download')(bot);
+	} catch (e) {
+		console.warn('Error downloading from github');
+		console.error(e);
+	}
+
+	try {
+		await require('../jenkins/download')(bot);
+	} catch (e) {
+		console.warn('Error downloading from jenkins');
+		console.error(e);
+	}
 };
